2a138be9b82390b7b5aa5cebcbbc57c2f3e93185
yyang22-AdvancedJava <yyang22@madisoncollege.edu>
2026-02-04 16:56:45 -0600

    Week 4 - Exercise - Project: TravelJournal - Completed

--- src/test/java/persistence/UserDaoTest.java
+++ src/test/java/persistence/UserDaoTest.java
@@ -1,7 +1,11 @@
 package persistence;
 
 import entity.User;
+
+import util.Database;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,35 +13,73 @@ class UserDaoTest {
 
     UserDao userDao;
 
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("cleanDB.sql");
+    }
+
     @Test
     void getById() {
         userDao = new UserDao();
         User retrievedUser = userDao.getById(1);
         assertNotNull(retrievedUser);
-        assertEquals("Joe", retrievedUser.getFirstName());
+        assertEquals("admin", retrievedUser.getUserName());
     }
 
     @Test
     void update() {
+        userDao = new UserDao();
+        User userToUpdate = userDao.getById(1);
+        userToUpdate.setLastName("Administrator");
+        userDao.update(userToUpdate);
+
+        // retrieve the user and check that the name change worked
+        User actualUser = userDao.getById(1);
+        assertEquals("Administrator", actualUser.getLastName());
+
     }
 
     @Test
     void insert() {
+        userDao = new UserDao();
+        User userToInsert = new User("Kia", "Yang", "ky001");
+        int insertedUserId = userDao.insert(userToInsert);
+        assertNotEquals(0, insertedUserId);
+        User insertedUser = userDao.getById(insertedUserId);
+        assertEquals("Kia", insertedUser.getFirstName());
     }
 
     @Test
     void delete() {
+        userDao = new UserDao();
+        userDao.delete(userDao.getById(2));
+        assertNull(userDao.getById(2));
+
     }
 
     @Test
     void getAll() {
+        userDao = new UserDao();
+        List<User> users = userDao.getAll();
+        assertEquals(7, users.size());
+
     }
 
     @Test
     void getByPropertyEqual() {
+        userDao = new UserDao();
+        List<User> users = userDao.getByPropertyLike("firstName", "Admin");
+        assertEquals(1, users.size());
+        assertEquals(1, users.get(0).getId());
+
     }
 
     @Test
     void getByPropertyLike() {
+        userDao = new UserDao();
+        List<User> users = userDao.getByPropertyLike("lastName", "c");
+        assertEquals(3, users.size());
+
     }
 }
